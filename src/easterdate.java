d1540a4d3b52ba4307a5a7ca74a98a53ccd2e4b0
domeix <domeix@web.de>
2016-12-14 14:20:00 +0100

    Ostern

---
+++ src/easterdate.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class easterdate {
+	public static void main(String[] Args) {
+		System.out.println("Bitte geben Sie die Jahreszahl zur Berechnung des Osterdatums ein:");
+		Scanner sc = new Scanner(System.in);
+		int y = sc.nextInt();
+		sc.close();
+		
+		if(y < 2100 && y >= 1900) {
+			int a = (y-1900)%19;
+			int b = (7*a+1)/19;
+			int m = (11*a+4-b)%29;
+			int q = (y-1900)/4;
+			int w = (y-1900+q+31-m)%7;
+			int d = 25-m-w;
+			if(d > 0) {
+				System.out.println("Ostersonntag im Jahr " + y + " am " + d + ". April.");
+			} else {
+				int d2 = 31+d;
+				System.out.println("Ostersonntag im Jahr " + y + " am " + d2 + ". März.");
+			}
+		} else {
+			System.out.println("Fehlerhafte Eingabe.");
+		}
+	}
+}
